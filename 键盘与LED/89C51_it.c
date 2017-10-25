be8bda860bb26d7bd97157b82f51369917e0514f
Meng2222 <Dreamer_Meng2@163.com>
2017-10-25 12:48:07 +0800

    增加了外部中断的初始化函数

--- 键盘与LED/89C51_it.c
+++ 键盘与LED/89C51_it.c
@@ -1,6 +1,33 @@
 #include "89C51_it.h"
 #include "timer.h"
 extern unsigned char LedItStatus;
+
+void ExternalITCmd(exITTypedef_t exITx, FunctionalState_t newState)
+{
+	if(exITx==exIT0)
+	{
+		EX0 = newState; 
+	}
+	else if(exITx==exIT1)
+	{
+		EX1 = newState;
+	}
+}
+
+void ExternalITInit(exITTypedef_t exITx , unsigned char triggerMode)
+{
+	if(exITx==exIT0)
+	{
+		IT0 = triggerMode;
+		ExternalITCmd(exIT0 , enable);
+	}
+	else if(exITx==exIT1)
+	{
+		IT1  = triggerMode;
+		ExternalITCmd(exIT1 , enable);		
+	}
+}
+
 //外部中断0中断服务函数
 void interint0()interrupt 0
 {
--- 键盘与LED/89C51_it.h
+++ 键盘与LED/89C51_it.h
@@ -1,4 +1,18 @@
 #ifndef __89C51_IT_H
 #define __89C51_IT_H
+#include "main.h"
+
+typedef enum
+{
+	exIT0,
+	exIT1
+}exITTypedef_t;
+
+#define EXIT_UPDOWN_TRIGGER (0x00)
+#define EXIT_DOWN_TRIGGER (0x01)
+
+void ExternalITCmd(exITTypedef_t exITx, FunctionalState_t newState);
+
+void ExternalITInit(exITTypedef_t exITx , unsigned char triggerMode);
 
 #endif
